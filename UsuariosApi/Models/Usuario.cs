bd29c9661b3b3516bc820312d66517c55d5dd6f1
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2025-08-17 22:25:50 -0300

    Adds more basic configs

--- UsuariosApi/Models/Usuario.cs
+++ UsuariosApi/Models/Usuario.cs
@@ -1,5 +1,12 @@
+using Microsoft.AspNetCore.Identity;
+
 namespace UsuariosApi.Models;
 
-public class Usuario
+public class Usuario : IdentityUser
 {
+    public DateTime DataNascimento { get; set; }
+
+    public Usuario() : base()
+    {
+    }
 }
--- UsuariosApi/obj/Debug/net9.0/UsuariosApi.AssemblyInfo.cs
+++ UsuariosApi/obj/Debug/net9.0/UsuariosApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UsuariosApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfaae22b7ee32af948eb772635ade93f009df8f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8332a2491f3108303a3511cb8f33d9a54db022e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("UsuariosApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UsuariosApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
