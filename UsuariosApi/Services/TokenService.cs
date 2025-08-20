638b612f0c6fc0504160b2eb26f28c7137a9354b
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2025-08-19 23:25:22 -0300

    Adds a token service

---
+++ UsuariosApi/Services/TokenService.cs
@@ -0,0 +1,11 @@
+using UsuariosApi.Models;
+
+namespace UsuariosApi.Services;
+
+public class TokenService
+{
+    public void GenerateToken(Usuario usuario)
+    {
+        throw new NotImplementedException();
+    }
+}
