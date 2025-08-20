7c18d4e450727f93fcd18d49fe88582cf28678d2
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2025-08-20 14:56:33 -0300

    TokenService

--- UsuariosApi/Services/TokenService.cs
+++ UsuariosApi/Services/TokenService.cs
@@ -1,11 +1,33 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
 using UsuariosApi.Models;
 
 namespace UsuariosApi.Services;
 
 public class TokenService
 {
-    public void GenerateToken(Usuario usuario)
+    public string GenerateToken(Usuario usuario)
     {
-        throw new NotImplementedException();
+        Claim[] claims = new Claim[]
+        {
+            new Claim("username", usuario.UserName),
+            new Claim("id", usuario.Id),
+            new Claim(ClaimTypes.DateOfBirth, usuario.DataNascimento.ToString()),
+        };
+
+        var chave = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes("antantomefinfantofearistatolescompastoreszigzagdotchissuguassu"));
+
+        var signingCredentials = new SigningCredentials(chave, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            expires: DateTime.Now.AddMinutes(10),
+            claims: claims,
+            signingCredentials: signingCredentials
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
     }
 }
--- UsuariosApi/Services/UsuarioService.cs
+++ UsuariosApi/Services/UsuarioService.cs
@@ -11,13 +11,16 @@ public class UsuarioService
     private IMapper _mapper;
     private UserManager<Usuario> _userManager;
     private SignInManager<Usuario> _signInManager;
+    private TokenService _tokenService;
 
 
-    public UsuarioService(IMapper mapper, UserManager<Usuario> userManager, SignInManager<Usuario> signInManager)
+    public UsuarioService(IMapper mapper, UserManager<Usuario> userManager, SignInManager<Usuario> signInManager,
+        TokenService tokenService)
     {
         _mapper = mapper;
         _userManager = userManager;
         _signInManager = signInManager;
+        _tokenService = tokenService;
     }
 
     public async Task Cadastra(CreateUsuarioDto dto)
@@ -45,5 +48,11 @@ public class UsuarioService
         {
             throw new ApplicationException("Usuário não autenticado.");
         }
+
+        var usuario =
+            _signInManager.UserManager.Users.FirstOrDefault(user => user.NormalizedUserName == dto.Username.ToUpper());
+
+
+        _tokenService.GenerateToken(usuario);
     }
 }
