dfa0ab5697cb27575291e51c40d5c010c7f7dbb3
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2025-08-21 18:03:28 -0300

    Fixes issues

--- UsuariosApi/Program.cs
+++ UsuariosApi/Program.cs
@@ -1,5 +1,9 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using UsuariosApi.Authorization;
 using UsuariosApi.Data;
 using UsuariosApi.Models;
@@ -24,20 +28,41 @@ builder.Services
 // Configures Automapper (the newest version, with the latest syntax)
 builder.Services.AddAutoMapper(cfg => { }, typeof(Program).Assembly);
 
-// Access Policy
-builder.Services.AddAuthorization(options =>
-{
-    options.AddPolicy("IdadeMinima", policy => { policy.AddRequirements(new IdadeMinima(18)); });
-});
-
 // Configs de injeção de dependência / instanciação para cada service usada nas controllers.
 builder.Services.AddScoped<UsuarioService>();
 builder.Services.AddScoped<TokenService>();
 
 
+// Access Policy
+builder.Services
+    .AddAuthentication(options => { options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme; })
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes("antantomefinfantofearistatolescompastoreszigzagdotchissuguassu")),
+            ValidateAudience = false,
+            ValidateIssuer = false,
+            ClockSkew = TimeSpan.Zero
+        };
+    });
+
+builder.Services.AddSingleton<IAuthorizationHandler, IdadeAuthorization>();
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("IdadeMinima", policy => { policy.AddRequirements(new IdadeMinima(18)); });
+});
+
+
 builder.Services.AddControllers();
+
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
 
 var app = builder.Build();
 
@@ -48,6 +73,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
